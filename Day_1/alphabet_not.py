79274402b18e805f71e37171d0ac74e2f1fe1e0c
ushaswi3 <ushaswireddy3@gmail.com>
2025-09-03 15:03:39 +0530

    python_day1

---
+++ Day_1/alphabet_not.py
@@ -0,0 +1,7 @@
+def alphabet_not():
+    a=input("Enter the charecter:")
+    if(a.isalpha()):
+        print("Character is an alphabet")
+    else:
+        print("not an alphabet")
+alphabet_not()

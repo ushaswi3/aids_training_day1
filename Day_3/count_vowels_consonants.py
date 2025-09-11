69df3d642e201c6f2fb6b21f74a508018be71861
ushaswi3 <ushaswireddy3@gmail.com>
2025-09-11 09:32:21 +0530

    modified_programs

--- Day_3/count_vowels_consonants.py
+++ Day_3/count_vowels_consonants.py
@@ -1,1 +1,13 @@
 '''to count total no.of vowels and consonats in a string'''
+
+def vowels_consonants(s):
+    c=v=0
+    for i in range(0,len(s)):
+        if(s[i].isalpha()):
+            if(s[i] in ['a','e','i','o','u']):
+                v+=1
+            else:
+                c+=1
+    print(s," has consonats:",c," vowels:",v)
+s=input("Enter the string:")
+vowels_consonants(s)
--- Day_3/fav_string_ques.py
+++ Day_3/fav_string_ques.py
@@ -1,2 +1,16 @@
 '''inpu: aaabbca
    output: a4b2c1'''
+
+def char_count(s):
+   v=[]
+   for i in range(0,len(s)):
+      if(s[i] not in v):
+         f=1
+         for j in range(i+1,len(s)):
+            if(s[i]==s[j]):
+               f+=1
+         print(s[i],f,sep="",end="")
+         v.append(s[i])
+
+s=input("Enter the string:")
+char_count(s)
